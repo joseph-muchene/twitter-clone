28fba53dd0b9b01619624df3b076a48ca3441f47
joseph muchene <ngugimuchene@gmail.com>
2022-06-01 13:11:30 +0300

    intergrated firebase

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD2q8jkRUZInu4ay7jK2zV2PltBDbTi77M",
+  authDomain: "twitter-11829.firebaseapp.com",
+  projectId: "twitter-11829",
+  storageBucket: "twitter-11829.appspot.com",
+  messagingSenderId: "568721727591",
+  appId: "1:568721727591:web:3f6bee37604997f75e34c2",
+  measurementId: "G-06SLE9P9XW",
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
---
+++ js/main.js
@@ -0,0 +1,35 @@
+const credentials = document.getElementById("credential");
+const closeBtn = document.getElementById("closeBtn");
+const signUp = document.getElementById("signUp");
+
+console.log(signUp);
+signUp.onclick = function () {
+  console.log("hello ");
+  credentials.style.visibility = "visible";
+};
+
+closeBtn.onclick = function () {
+  credentials.style.visibility = "hidden";
+};
+// register
+const registerBtn = document.getElementById("registerbtn");
+
+registerBtn.onclick = () => {
+  const name = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // send user to the database
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredentials) => {
+      let user = userCredentials.user;
+      if (user) {
+        // redirect the user to the home page
+
+        window.location.href = "../views/home.html";
+      }
+    });
+};
